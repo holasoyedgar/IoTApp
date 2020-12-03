38f7f2c01b69ce5e1bebfee19732fd5435a7064f
Edgar Rincon <eers222@gmail.com>
2020-12-03 05:07:19 -0600

    Production environment variables were added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://iot-server-edgar-rincon.herokuapp.com/v1/api',
+  ARDUINO_KEY: '84b5b2ffecdb25fa8c50faff7556da32',
 };
